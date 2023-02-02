8fa3a1af6a3cb0c8fb1377218b07b55fd84095e4
James Marke ⠕ <72310196+eggySHECAH@users.noreply.github.com>
2023-02-02 11:55:48 +0000

    yes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 class AyoYourMom:
     def __init__(self):
-        input.on_button_pressed(Button.A, a)
-    def a(self):
         num1 = 5
         num2 = 6
         num3 = num1 + num2
         print(num3)
+
+aym = AyoYourMom()
